cb3ea7e5e7d1b20451a55f5120fad4f5b3f20784
Zak J <78830805+zakj1968@users.noreply.github.com>
2021-04-16 09:23:58 +0800

    Update azan_on_ESP32.ino
    Remove second from fetching time

--- azan_on_ESP32.ino
+++ azan_on_ESP32.ino
@@ -414,7 +414,7 @@ void loop()
   {
     String payloadStr;
     PrData prData2;
-	if ((rtctime.Hour() == 1 && rtctime.Minute() == 0 && rtctime.Second() == 0) || (rtctime.Hour() == 12 && rtctime.Minute() == 0 && rtctime.Second() == 0) )
+	if ((rtctime.Hour() == 1 && rtctime.Minute() == 0) || (rtctime.Hour() == 12 && rtctime.Minute() == 0))
 	{
 	  fetchOnce = true;    
 	} 
